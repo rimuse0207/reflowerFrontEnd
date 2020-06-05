fda109446b4f4b6242fd060108ff2c66fffd73eb
sleny <xkqortjddl@naver.com>
2020-06-05 12:59:39 +0900

    asdasd

---
+++ build/precache-manifest.611608563b7bc41304ccc08d8f124948.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7d31b7b485809869f45ef0f1ff046a8",
+    "url": "/reflowerFrontEnd/index.html"
+  },
+  {
+    "revision": "4a8c73900c3534522190",
+    "url": "/reflowerFrontEnd/static/css/2.ecffae5e.chunk.css"
+  },
+  {
+    "revision": "b59b506b9d673680ea25",
+    "url": "/reflowerFrontEnd/static/css/main.cccadd02.chunk.css"
+  },
+  {
+    "revision": "4a8c73900c3534522190",
+    "url": "/reflowerFrontEnd/static/js/2.fc15413d.chunk.js"
+  },
+  {
+    "revision": "78e572a38ce62fad1af1cd75e03f15bb",
+    "url": "/reflowerFrontEnd/static/js/2.fc15413d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b59b506b9d673680ea25",
+    "url": "/reflowerFrontEnd/static/js/main.643504ad.chunk.js"
+  },
+  {
+    "revision": "c7e0743d476bc4270280",
+    "url": "/reflowerFrontEnd/static/js/runtime-main.bbc6a9ee.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ed5d1a4e66626fdb861563338ecaa706.js"
+  "/reflowerFrontEnd/precache-manifest.611608563b7bc41304ccc08d8f124948.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/reflowerFrontEnd/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
